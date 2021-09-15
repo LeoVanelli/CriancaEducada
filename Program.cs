b3f1f3ce1855e7cab0003e5825040a647c25080c
Leandro Gonçalves Vanelli <55560279+LeoVanelli@users.noreply.github.com>
2021-09-14 21:27:28 -0300

    Adiciona atividade CriançaEducada

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CriancaEducada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Solicite que o usuário digite uma frase. Substitua as palavras da lista por uma sequência de símbolos (como `#@$%*!&`, por exemplo).
+
+            //chato/chata, bobo/boba, feio/feia, boboca, bocó, tonto/tonta, palerma, paspalho/paspalha, tantã, panaca, pentelho/pentelha, burro/burra, besta
+
+            string[] palavras = new string[] {"chato","chata","bobo","boba","feio","feia","boboca","bocó","tonto","tonta","palerma","paspalho","paspalha","tantã","panaca","pentelho","pentelha","burro","burra","besta"};
+            
+            Console.Write("Me diga o que você pensa sobre o aluno que não faz a lição de casa: ");           
+            
+            string texto = Console.ReadLine();
+
+            string[] textoSeparado = texto.Split(' ');
+
+            Console.WriteLine("\nEu sou uma criança educada, no seu lugar eu diria:");
+
+            for (int i = 0; i < textoSeparado.Length; i++) // Vai procurar a quantidade de palavras do texto
+            {
+                foreach (string palavra in palavras)
+                {
+                    if(textoSeparado[i] == palavra){
+                        textoSeparado[i] = "#@$%*!&"; // Separa os caracteres da palavra para censura-la
+                    }
+                }
+                Console.Write("{0} ",textoSeparado[i]);
+            }
+        }
+    }
+}
